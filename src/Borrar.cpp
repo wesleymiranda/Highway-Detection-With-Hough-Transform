1b871de5ae0153d1edc93577085d217ce7c7f679
wesleymirada <wymirandac@gmail.com>
2019-04-28 00:00:00 -0400

    Transformada de Hough

--- src/Borrar.cpp
+++ src/Borrar.cpp
@@ -26,3 +26,25 @@ uchar Media(Mat A) {
 	return (uchar)(sum / 3.8);
 	return 0;
 }
+
+Mat borrar(Mat M, int cont) {
+	Mat Aux;
+	M.copyTo(Aux);
+	int i = 0;
+
+	while(i<cont)
+	{
+		for (int lin = 1; lin < Aux.rows - 1; lin++) 
+		{
+			for (int col = 1; col < Aux.cols - 1; col++)
+			{
+				Mat Temp = M(Range(lin - 1, lin + 2), Range(col - 1, col + 2)); //Operador que extrai uma submatriz da matriz original
+				Aux.at<uchar>(lin, col) = Media(Temp);
+			}
+		}
+	Aux.copyTo(M);
+	i++;
+	}
+
+	return Aux;
+}
